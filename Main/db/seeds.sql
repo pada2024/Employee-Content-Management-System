e60c8aa7f36d0903995e82a65e5f988c78765c14
Parisa Daily <misssweetardor@yahoo.com>
2024-09-22 15:32:41 -0700

    Added starter code

---
+++ Main/db/seeds.sql
@@ -0,0 +1,61 @@
+npmINSERT INTO employee_first_name (first_name)
+VALUES ('John'),
+       ('Mike'),
+       ('Ashley'),
+       ('Kevin'),
+       ('Kunal'),
+       ('Malia'),
+       ('Sarah'),
+       ('Tom');
+
+npmINSERT INTO employee_last_name (last_name)
+VALUES ('Doe'),
+       ('Chan'),
+       ('Rodriguez'),
+       ('Tupik'),
+       ('Singh'),
+       ('Brown'),
+       ('Lourd'),
+       ('Allen');
+
+npmINSERT INTO employee_title (title)
+VALUES ('Sales Lead'),
+       ('Salesperson'),
+       ('Lead Engineer'),
+       ('Software Engineer'),
+       ('Account Manager'),
+       ('Accountant'),
+       ('Legal Team Lead'),
+       ('Lawyer');
+       
+npmINSERT INTO employee_department (department)
+VALUES ('Sales'),
+       ('Sales'),
+       ('Engineering'),
+       ('Engineering'),
+       ('Finance'),
+       ('Finance'),
+       ('Legal'),
+       ('Legal');
+
+npmINSERT INTO employee_salary (salary)
+VALUES (100000),
+       (80000),
+       (150000),
+       (120000),
+       (160000),
+       (125000),
+       (250000),
+       (190000);
+       
+           
+
+INSERT INTO employee_reviews (movie_id, review)
+VALUES (1, 'Zazu is underrated. Give that hornbill a sequel!'),
+       (2, 'I''m gonna make him an offer you can''t refuse, watch this movie'),
+       (1, 'Scar is the lion everyone loves to hate'),
+       (3, 'Ten years of ballet and three years of tap to join a gang in this neighborhood'),
+       (5, 'The tin man gave a metallic, hollow performance'),
+       (1, 'Hakuna matata'),
+       (5, 'Those flying monkeys are nightmare fuel!');
+       
