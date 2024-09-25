83731e9b15ae083e2b890b436df7e4b91da13247
Parisa Daily <misssweetardor@yahoo.com>
2024-09-24 20:00:55 -0700

    Worked with tutor to help me refacter DB and index.js so my prompts would actually connect to my DB

--- index.js
+++ index.js
@@ -7,69 +7,89 @@ const { Pool } = require('pg');
 require("dotenv").config();
 const inquirer = require('inquirer');
 
-const actionQuestions = [{
-  name: "action",
-  type: "list",
-  message: "Welcome! What would you like to do?",
-  choices: [
+let db;
+
+async function connectDb() {
+  // Connect to database
+  const pool = new Pool(
+    {
+      // TODO: Enter PostgreSQL username
+      user: process.env.USER,
+      // TODO: Enter PostgreSQL password
+      password: process.env.PASSWORD,
+      host: '127.0.0.1',
+      database: 'employee_db',
+      port: 5432
+    },
+    console.log(`Connected to the employees_db database.`)
+  )
+
+  db = await pool.connect()
+}
+
+async function mainMenu () {
+
+  const actionQuestions = [{
+    name: "action",
+    type: "list",
+    message: "Welcome! What would you like to do?",
+    choices: [
+      "View all Departments",
+      "View all Roles",
       "View all Employees",
       "Add an Employee",
       "Exit"
-  ]
-}];
-
-inquirer
-  .prompt(actionQuestions)
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    // connect to DB
-    // query for all emplpyees
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-
-
-// Connect to database
-const pool = new Pool(
-  {
-    // TODO: Enter PostgreSQL username
-    user: process.env.USER,
-    // TODO: Enter PostgreSQL password
-    password: process.env.PASSWORD,
-    host: 'localhost',
-    database: 'employees_db'
-  },
-  console.log(`Connected to the employees_db database.`)
-)
-
-pool.connect();
-
-// Create a movie
-
-const sql = `INSERT INTO movies (movie_name)
-    VALUES ($1)`;
-const params = [body.movie_name];
-
-pool.query(sql, params, (err, result) => {
-  if (err) {
-
-    return;
-  }
-
-
-});
+    ]
+  }];
+  
+  inquirer
+    .prompt(actionQuestions)
+    .then((answers) => {
+  
+      if (answers.action == "View all Departments") {
+        // const sql = `INSERT INTO movies (movie_name)
+        // VALUES ($1)`;
+        const sql = "SELECT * FROM department";
+        // const params = [body.movie_name];
+  
+        db.query(sql, (err, result) => {
+          if (err) {
+            console.log(err)
+            return;
+          } else {
+            console.table(result.rows)
+            mainMenu();
+          }
+        });
+  
+      }
+      else if (answers.action == "View all Roles") {
+        // const sql = `INSERT INTO movies (movie_name)
+        // VALUES ($1)`;
+        const sql = "SELECT * FROM role";
+        // const params = [body.movie_name];
+  
+        db.query(sql, (err, result) => {
+          if (err) {
+            console.log(err)
+            return;
+          } else {
+            console.table(result.rows)
+            mainMenu();
+          }
+        });
+  
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+async function init() {
+  await connectDb();
+  mainMenu();
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+init()
